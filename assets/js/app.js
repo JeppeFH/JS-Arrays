0e454b56b9c35e49923d665e5d74d7eb8219996a
JeppeFH <jeppefinch@hotmail.dk>
2024-08-09 09:16:11 +0200

    update js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,43 @@
+/*Variabler*/
+const girls = document.querySelectorAll(".output-girls");
+const boys = document.querySelectorAll(".output-boys");
+const both = document.querySelectorAll(".output-both");
 
+/*Arrays*/
+let arrayGirls = [
+  "Alm",
+  "Ida",
+  "Clara",
+  "Olivia",
+  "Freja",
+  "Emma",
+  "Luna",
+  "Agnes",
+  "Nora",
+];
+
+let arrayBoys = [
+  "Oscar",
+  "Karl",
+  "William",
+  "Oliver",
+  "Alfred",
+  "August",
+  "Valdemar",
+  "Malthe",
+  "Arthur",
+  "Emil",
+];
+
+let arrayBoth = [
+  "Alpha",
+  "Alfie",
+  "Andy",
+  "Andrea",
+  "Angel",
+  "Antonie",
+  "Bobbie",
+  "Benny",
+  "Billie",
+  "Björk",
+];
